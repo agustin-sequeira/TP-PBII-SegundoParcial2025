3554411d6b9b5a4e21100eaa095c5595a7f242b8
agustin-sequeira <agusseq@gmail.com>
2025-11-19 00:15:18 -0300

    1erCommit
    creacion de repositorio y criaturaTest

---
+++ test/ar/edu/unlam/TP_PBII_SegundoParcial2025/CriaturaTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.TP_PBII_SegundoParcial2025;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CriaturaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
