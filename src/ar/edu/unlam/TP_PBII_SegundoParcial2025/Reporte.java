ed8000c1d36efbcdaa7c55f33884458ee2d6f359
agustin-sequeira <agusseq@gmail.com>
2025-11-22 03:11:27 -0300

    creacion de Reporte punto 1

---
+++ src/ar/edu/unlam/TP_PBII_SegundoParcial2025/Reporte.java
@@ -0,0 +1,12 @@
+package ar.edu.unlam.TP_PBII_SegundoParcial2025;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unlam.TP_PBII_SegundoParcial2025.criatura.Criatura;
+
+public class Reporte {
+	List <String> listadoDeCriaturas = new ArrayList();
+	
+	
+}
