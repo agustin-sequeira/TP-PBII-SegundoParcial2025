16d545f5d2e8ad8694cb61f7383c60a146f88129
agustin-sequeira <agusseq@gmail.com>
2025-11-22 18:00:28 -0300

    reporte final
    los 4 puntos estan completos

--- src/ar/edu/unlam/TP_PBII_SegundoParcial2025/Reporte.java
+++ src/ar/edu/unlam/TP_PBII_SegundoParcial2025/Reporte.java
@@ -1,12 +1,69 @@
 package ar.edu.unlam.TP_PBII_SegundoParcial2025;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import ar.edu.unlam.TP_PBII_SegundoParcial2025.criatura.Criatura;
+import ar.edu.unlam.TP_PBII_SegundoParcial2025.criatura.CriaturaElemental;
+import ar.edu.unlam.TP_PBII_SegundoParcial2025.enumerable.AfinidadElemental;
 
 public class Reporte {
-	List <String> listadoDeCriaturas = new ArrayList();
+
+	public List<CriaturaElemental> listadoTotalDeCriaturas(List<MaestroElemental> maestros) {
+		List<CriaturaElemental> resultado = new ArrayList();
+		for (MaestroElemental maestro : maestros) {
+			resultado.addAll(maestro.obtenerCriaturas());
+		}
+		
+		return resultado;
+	}
+
+	public CriaturaElemental devuelveCriaturaConMayorEnergiaTotal(List<MaestroElemental> maestros) {
+		CriaturaElemental criaturaConMayorEnergia = null;
+		Integer mayorEnergia = 0;
+		
+		for(MaestroElemental maestro : maestros) {
+			for (CriaturaElemental criatura : maestro.obtenerCriaturas()) {
+				if (criatura.getNivelDeEnergia() > mayorEnergia) {
+					mayorEnergia = criatura.getNivelDeEnergia();
+					criaturaConMayorEnergia = criatura;
+				};
+			}	
+		}
+		return criaturaConMayorEnergia;
+	}
+
+	public MaestroElemental maestroConMasCriaturasTransformadas(List<MaestroElemental> maestros) {
+		MaestroElemental maestroConMasTransformaciones = null ;
+		Integer maximaCantidadDeCriaturasTransformadas= 0;
+		
+		for(MaestroElemental maestro : maestros){
+			Integer transformaciones = maestro.contarCriaturasTransformadas();
+			if(transformaciones > maximaCantidadDeCriaturasTransformadas) {
+				maestroConMasTransformaciones = maestro;
+				maximaCantidadDeCriaturasTransformadas = transformaciones;
+			}
+		} 
+		return maestroConMasTransformaciones;
+	}
+
+	public HashMap<AfinidadElemental, Integer> cantidadDeCriaturasPorAfinidad(List<MaestroElemental> maestros) {
+		HashMap<AfinidadElemental, Integer> mapa = new HashMap();
+		
+		for(AfinidadElemental afinidad : AfinidadElemental.values()) {
+			mapa.put(afinidad, 0);
+			
+		}
+		
+		for(MaestroElemental maestro : maestros) {
+			for(CriaturaElemental criatura : maestro.obtenerCriaturas()) {
+				mapa.put(criatura.getAfinidadPrimaria(), mapa.get(criatura.getAfinidadPrimaria()) + 1);
+			}
+		}
+		return mapa;
+	}
+	
 	
 	
 }
--- test/ar/edu/unlam/TP_PBII_SegundoParcial2025/ReporteTest.java
+++ test/ar/edu/unlam/TP_PBII_SegundoParcial2025/ReporteTest.java
@@ -1,6 +1,12 @@
 package ar.edu.unlam.TP_PBII_SegundoParcial2025;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -27,9 +33,102 @@ public class ReporteTest {
 		syl.registrarCriatura(domesticada);
 		syl.registrarCriatura(ancestral);
 		
+		List<MaestroElemental> maestros = new ArrayList();
+		maestros.add(miMaestro);
+		maestros.add(syl);
+		
 		Reporte miReporte = new Reporte();
 		
-		/*assertTrue();*/
+		List<CriaturaElemental> criaturas = miReporte.listadoTotalDeCriaturas(maestros);
+		
+		
+		assertEquals(3, criaturas.size());
+		assertTrue(criaturas.contains(miCriatura));
+		assertTrue(criaturas.contains(domesticada));
+		assertTrue(criaturas.contains(ancestral));
+		
+		
+	}
+	
+	@Test
+	public void registraYDevuelveLaCriaturaConMayorEnergiaTotal() {
+		Criatura miCriatura = new CriaturaSalvaje("Charizard", 100, AfinidadElemental.FUEGO, EstadoEmocional.INSTABLE);
+		Criatura domesticada = new CriaturaDomesticada("Coral", 50, AfinidadElemental.AGUA, EstadoEmocional.TRANQUILA);
+		Criatura ancestral = new CriaturaAncestral("Ancestro", 120, AfinidadElemental.TIERRA, EstadoEmocional.TRANQUILA);
+		
+		MaestroElemental miMaestro = new MaestroElemental("Maestro Yoda", 25, AfinidadElemental.AIRE);
+		MaestroElemental syl = new MaestroElemental("Maestra Syl", 15, AfinidadElemental.AIRE);
+		
+		miMaestro.registrarCriatura(miCriatura);
+		syl.registrarCriatura(domesticada);
+		syl.registrarCriatura(ancestral);
+		
+		List<MaestroElemental> maestros = new ArrayList();
+		maestros.add(miMaestro);
+		maestros.add(syl);
+		
+		Reporte miReporte = new Reporte();
+		
+		
+		CriaturaElemental criaturaConMayorEnergia = miReporte.devuelveCriaturaConMayorEnergiaTotal(maestros);
+		
+		assertEquals(120, criaturaConMayorEnergia.getNivelDeEnergia());
+	
+		
+	}
+	
+	@Test
+	public void devuelveMaestroConMasCriaturasTransformadas() {
+		Criatura domesticada = new CriaturaDomesticada("Coral", 50, AfinidadElemental.AGUA, EstadoEmocional.TRANQUILA);
+		Criatura ancestral = new CriaturaAncestral("Ancestro", 120, AfinidadElemental.TIERRA, EstadoEmocional.TRANQUILA);
+		
+		MaestroElemental miMaestro = new MaestroElemental("Maestro Yoda", 25, AfinidadElemental.AIRE);
+		MaestroElemental syl = new MaestroElemental("Maestra Syl", 15, AfinidadElemental.AGUA);
+		
+		miMaestro.registrarCriatura(domesticada);
+		syl.registrarCriatura(ancestral);
+		
+		miMaestro.aplicarBendicionDelRio(domesticada.getNombre());
+		miMaestro.aplicarLlamaInterna(ancestral.getNombre());
+		
+		
+		Reporte miReporte = new Reporte();
+		
+		List<MaestroElemental> maestros = new ArrayList();
+		maestros.add(miMaestro);
+		maestros.add(syl);
+		MaestroElemental resultado = miReporte.maestroConMasCriaturasTransformadas(maestros);
+		
+		assertEquals(miMaestro, resultado);
+	
+		
+	}
+	
+	@Test
+	public void devuelveCantidadDeCriaturasPorAfinidadElemental() {
+		Criatura miCriatura = new CriaturaSalvaje("Charizard", 100, AfinidadElemental.FUEGO, EstadoEmocional.INSTABLE);
+		Criatura domesticada = new CriaturaDomesticada("Coral", 50, AfinidadElemental.AGUA, EstadoEmocional.TRANQUILA);
+		Criatura ancestral = new CriaturaAncestral("Ancestro", 120, AfinidadElemental.TIERRA, EstadoEmocional.TRANQUILA);
+		
+		MaestroElemental miMaestro = new MaestroElemental("Maestro Yoda", 25, AfinidadElemental.AIRE);
+		MaestroElemental syl = new MaestroElemental("Maestra Syl", 15, AfinidadElemental.AGUA);
+		
+		miMaestro.registrarCriatura(miCriatura);
+		miMaestro.registrarCriatura(domesticada);
+		syl.registrarCriatura(ancestral);
+		
+		Reporte miReporte = new Reporte();
+		List<MaestroElemental> maestros = new ArrayList();
+		maestros.add(miMaestro);
+		maestros.add(syl);
+		HashMap<AfinidadElemental, Integer> mapa = miReporte.cantidadDeCriaturasPorAfinidad(maestros);
+		
+		assertEquals(1, (int) mapa.get(AfinidadElemental.FUEGO));
+		assertEquals(1, (int) mapa.get(AfinidadElemental.AGUA));
+		assertEquals(1, (int) mapa.get(AfinidadElemental.TIERRA));
+		assertEquals(0, (int) mapa.get(AfinidadElemental.AIRE));
+	
+		
 	}
 
 
